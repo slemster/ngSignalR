ad70d22a522c472fc75232368e8ef4beb2d1f483
Sean Lemster <sean.lemster@gmail.com>
2018-12-21 08:37:25 -0800

    Add SignalR NuGet Package Add Message WebAPI endpoint

---
+++ SignalR-Hub/NotifyHub.cs
@@ -0,0 +1,9 @@
+﻿
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRHub
+{
+    public class NotifyHub : Hub<ITypedHubClient>
+    {
+    }
+}
--- SignalR-Hub/Startup.cs
+++ SignalR-Hub/Startup.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using SignalRHub;
+
 
 namespace SignalR_Hub
 {
@@ -25,6 +20,14 @@ namespace SignalR_Hub
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+             services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>{
+                 builder
+                     .AllowAnyMethod()
+                     .AllowAnyHeader()
+                     .WithOrigins("http://localhost:4200");
+            }));
+            services.AddSignalR();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -40,6 +43,12 @@ namespace SignalR_Hub
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseCors("CorsPolicy");
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<NotifyHub>("/notify");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
